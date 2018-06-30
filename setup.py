4f5fcb016da939592944f4f5a9db9b89e42808f5
Julian Niedermeier <jpniedermeier@gmail.com>
2018-06-30 13:45:01 +0200

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='pycocoevalcap',
       version='1.0',
       packages=['pycocoevalcap', 'pycocoevalcap.tokenizer',
-                'pycocoevalcap.rogue', 'pycocoevalcap.meteor',
+                'pycocoevalcap.rouge', 'pycocoevalcap.meteor',
                 'pycocoevalcap.cider', 'pycocoevalcap.bleu'],
       package_dir={'pycocoevalcap': 'pycocoevalcap'}
       )
