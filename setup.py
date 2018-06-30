3dbf9f4b010edd803eb42f171092de9bea28ec87
Julian Niedermeier <jpniedermeier@gmail.com>
2018-06-30 15:36:56 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='pycocoevalcap',
       version='1.0',
-      packages=find_packages()
+      packages=find_packages(),
+      include_package_data=True
       )
